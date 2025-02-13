eaa63dcfe9534094c110ff6a15caac7b95b42077
Eirik Bolstad <eirik@like-it.no>
2025-02-13 14:37:01 +0100

    Remove cloud block

--- main.tf
+++ main.tf
@@ -21,9 +21,24 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-central-1a"
+}
+
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+  subnet_id     = aws_subnet.my_subnet.id  # Use the new subnet
+   root_block_device {
+    volume_type = "gp3"
+    volume_size = 10  # Adjust size as needed
+  }
 
   tags = {
     Name = var.instance_name
--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,13 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
+ /*  cloud {
+    organization = "bolstad"
 
     workspaces {
       name = "learn-terraform"
     }
-  }
+  } */
 
   required_providers {
     aws = {
